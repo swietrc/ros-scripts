6a5b677bff262cc849b86bdcfb9bf025e5ecc368
root <swietrich@etu.unistra.fr>
2018-01-31 16:28:22 +0100

    up

--- demo_twist/src/move.py
+++ demo_twist/src/move.py
@@ -1,28 +1,81 @@
 #!/usr/bin/env python
+
 import rospy
 from geometry_msgs.msg import Twist
 
-speed = 1
-distance = 1
 
-def move():
+class RobotMove:
+    DRIVE_SPEED = 0.2
+    DRIVE_SPEED_FACTOR = 0.80
+    ROT_SPEED = 1.2
+    ROT_SPEED_FACTOR = 0.7
+    CMD_VEL_TIME_FACTOR = 0.625
+    
     # Start new node
     rospy.init_note('robot_twist', anonymous=True)
-    velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=10)
+    cmd_vel = rospy.Publisher('cmd_vel', Twist, queue_size=10)
+
+def line():
     vel_msg = Twist()
     
-    vel_msg.linear.x = speed
-    vel_msg.linear.y = 0
-    vel_msg.linear.z = 0
+    vel_msg.linear.x = 0.3
+    _move_wheels(3, vel_msg)
 
-    vel_msg.angular.x = 0
-    vel_msg.angular.y = 0
-    vel_msg.angular.z = 0
+def square():
+    # Start new node
+    rospy.init_note('robot_twist', anonymous=True)
+    vel_msg = Twist()
+    
+    vel_msg.linear.x = 0.3
+    _move_wheels(3, vel_msg)
 
-    velocity_publisher.publish(vel_msg)
+    for i in range(1,4)
 
-    vel_msg.linear.x = 0
-    velocity_publisher.publish(vel_msg)
+	vel_msg = Twist()
+
+	vel_msg.angular.x = 1,57*i
+	_move_wheels(1, vel_msg)
+
+	vel_msg = Twist()
+    
+	vel_msg.linear.x = 0.3
+	_move_wheels(3, vel_msg)
+
+
+def triangle():
+    # Start new node
+    rospy.init_note('robot_twist', anonymous=True)
+    vel_msg = Twist()
+    
+    vel_msg.linear.x = 0.3
+    _move_wheels(3, vel_msg)
+
+    for i in range(1,3)
+
+	vel_msg = Twist()
+
+	vel_msg.angular.x = 1,05*i
+	_move_wheels(1, vel_msg)
+
+	vel_msg = Twist()
+    
+	vel_msg.linear.x = 0.3
+	_move_wheels(3, vel_msg)
+
+
+def _move_wheels(time, twist):
+    """Move wheels for 'time' with 'twist'.
+
+    :type twist: Twist
+    """
+    num = int(time / self.CMD_VEL_TIME_FACTOR)
+
+    for _ in range(num):
+	velocity_publisher.publish(twist)
+	rospy.sleep(self.CMD_VEL_TIME_FACTOR)
+
+    # stop
+    velocity_publisher.publish(Twist())
 
 if __name__ == '__main__':
     move()
