9813aa850a47d3041377c8fd4efb76e8daf4530a
root <swietrich@etu.unistra.fr>
2018-04-14 16:58:18 +0200

    lol.png

--- nav_eval/main_mvt.py
+++ nav_eval/main_mvt.py
@@ -13,14 +13,14 @@
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation; either version 2 of the License, or
     (at your option) any later version.5
-    
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details at:
-    
+
     http://www.gnu.org/licenses/gpl.html
-      
+
 """
 
 import take_photo
@@ -52,34 +52,34 @@ class NavTest():
     CMD_VEL_TIME_FACTOR = 0.625
 
     def __init__(self):
-        
-        
+
+
         rospy.init_node('nav_test', anonymous=True)
-        
+
         rospy.Subscriber("mouvement_zigbee", String, self.callback)
 
 
         rospy.on_shutdown(self.shutdown)
-        
+
         # How long in seconds should the robot pause at each location?
         self.rest_time = rospy.get_param("~rest_time", 10)
-        
+
         # Are we running in the fake simulator?
         self.fake_test = rospy.get_param("~fake_test", False)
-        
+
         # Goal state return values
-        goal_states = ['PENDING', 'ACTIVE', 'PREEMPTED', 
+        goal_states = ['PENDING', 'ACTIVE', 'PREEMPTED',
                        'SUCCEEDED', 'ABORTED', 'REJECTED',
                        'PREEMPTING', 'RECALLING', 'RECALLED',
                        'LOST']
-        
-        # Set up the goal locations. Poses are defined in the map frame.  
+
+        # Set up the goal locations. Poses are defined in the map frame.
         # An easy way to find the pose coordinates is to point-and-click
         # Nav Goals in RViz when running in the simulator.
         # Pose coordinates are then displayed in the terminal
         # that was used to launch RViz.
         locations = dict()
-        
+
         locations['depart'] = Pose(Point(2.787, 0.125, 0.000), Quaternion(0.000, 0.000, 0.223, 0.975))
         locations['porte'] = Pose(Point(-0.908, 0.234, 0.000), Quaternion(0.000, 0.000, -0.670, 0.743))
         locations['porte_finale'] = Pose(Point(-2.121, -5.865, 0.000), Quaternion(0.000, 0.000, 0.733, 0.680))
@@ -87,21 +87,21 @@ class NavTest():
 
         # Publisher to manually control the robot (e.g. to stop it)
         self.cmd_vel_pub = rospy.Publisher('cmd_vel', Twist)
-        
+
         # Subscribe to the move_base action server
         self.move_base = actionlib.SimpleActionClient("move_base", MoveBaseAction)
-        
+
         rospy.loginfo("Waiting for move_base action server...")
-        
+
         # Wait 60 seconds for the action server to become available
         self.move_base.wait_for_server(rospy.Duration(60))
-        
+
         rospy.loginfo("Connected to move base server")
-        
-        # A variable to hold the initial pose of the robot to be set by 
+
+        # A variable to hold the initial pose of the robot to be set by
         # the user in RViz
         initial_pose = PoseWithCovarianceStamped()
-        
+
         # Variables to keep track of success rate, running time,
         # and distance traveled
         n_locations = len(locations)
@@ -113,19 +113,19 @@ class NavTest():
         running_time = 0
         location = ""
         last_location = ""
-        
+
         # Get the initial pose from the user
         rospy.loginfo("*** Click the 2D Pose Estimate button in RViz to set the robot's initial pose...")
         rospy.wait_for_message('initialpose', PoseWithCovarianceStamped)
         self.last_location = Pose()
         rospy.Subscriber('initialpose', PoseWithCovarianceStamped, self.update_initial_pose)
-        
+
         # Make sure we have the initial pose
         while initial_pose.header.stamp == "":
             rospy.sleep(1)
-            
+
         rospy.loginfo("Starting navigation test")
-        
+
         # Begin the main loop and run through a sequence of locations
         while not rospy.is_shutdown():
             if movement_detected:
@@ -136,13 +136,13 @@ class NavTest():
                 self.goal.target_pose.pose = capteur_zigbee
                 self.goal.target_pose.header.frame_id = 'map'
                 self.goal.target_pose.header.stamp = rospy.Time.now()
-		
+
                 rospy.loginfo("Going to the capteur_zigbee")
                 self.move_base.send_goal(self.goal)
 
                 # Allow 5 minutes to get there
-                finished_within_time = self.move_base.wait_for_result(rospy.Duration(300)) 
-                
+                finished_within_time = self.move_base.wait_for_result(rospy.Duration(300))
+
                 # Check for success or failure
                 if not finished_within_time:
                     self.move_base.cancel_goal()
@@ -156,52 +156,46 @@ class NavTest():
                         rospy.loginfo("State:" + str(state))
                     else:
                         rospy.loginfo("Goal to the movement failed with error code: " + str(goal_states[state]))
-                
+
                 # Print a summary success/failure, distance traveled and time elapsed
                 rospy.sleep(self.rest_time)
-		
-		camera = take_photo.TakePhoto()
+                camera = take_photo.TakePhoto()
 
                 # Take a photo
 
                 # Use '_image_title' parameter from command line
                 # Default value is 'photo.jpg'
                 img_title1 = rospy.get_param('~image_title1', 'photo1.jpg')
-		rospy.loginfo("Je commence la rotation")
                 robot_move = square.RobotMove()
                 robot_move._rotate(math.pi/2)
-                rospy.loginfo("J'ai fini la rotation")
-                img_title2 = rospy.get_param('~image_title2', 'photo2.jpg')
-		robot_move = square.RobotMove()
-                robot_move._rotate(math.pi/2)
-                img_title3 = rospy.get_param('~image_title3', 'photo3.jpg')
-		robot_move = square.RobotMove()
-                robot_move._rotate(math.pi/2)
-                img_title4 = rospy.get_param('~image_title4', 'photo4.jpg')
-
-
-
                 if camera.take_picture(img_title1):
                     rospy.loginfo("Saved image " + img_title1)
                 else:
                    rospy.loginfo("No images received")
 
+                img_title2 = rospy.get_param('~image_title2', 'photo2.jpg')
+                robot_move = square.RobotMove()
+                robot_move._rotate(math.pi/2)
                 if camera.take_picture(img_title2):
                     rospy.loginfo("Saved image " + img_title2)
                 else:
                    rospy.loginfo("No images received")
 
+                img_title3 = rospy.get_param('~image_title3', 'photo3.jpg')
+                robot_move = square.RobotMove()
+                robot_move._rotate(math.pi/2)
                 if camera.take_picture(img_title3):
                     rospy.loginfo("Saved image " + img_title3)
                 else:
                    rospy.loginfo("No images received")
 
+                img_title4 = rospy.get_param('~image_title4', 'photo4.jpg')
                 if camera.take_picture(img_title4):
                     rospy.loginfo("Saved image " + img_title4)
                 else:
-		    rospy.loginfo("No images received")
+		            rospy.loginfo("No images received")
 
-    
+
             else:
                 # If we've gone through the current sequence,
                 # start with a new random sequence
@@ -212,47 +206,47 @@ class NavTest():
                     # the last location
                     if sequence[0] == last_location:
                         i = 1
-                
+
                 # Get the next location in the current sequence
                 location = sequence[i]
-                            
+
                 # Keep track of the distance traveled.
                 # Use updated initial pose if available.
                 if initial_pose.header.stamp == "":
-                    distance = sqrt(pow(locations[location].position.x - 
+                    distance = sqrt(pow(locations[location].position.x -
                                         locations[last_location].position.x, 2) +
-                                    pow(locations[location].position.y - 
+                                    pow(locations[location].position.y -
                                         locations[last_location].position.y, 2))
                 else:
                     rospy.loginfo("Updating current pose.")
-                    distance = sqrt(pow(locations[location].position.x - 
+                    distance = sqrt(pow(locations[location].position.x -
                                         initial_pose.pose.pose.position.x, 2) +
-                                    pow(locations[location].position.y - 
+                                    pow(locations[location].position.y -
                                         initial_pose.pose.pose.position.y, 2))
                     initial_pose.header.stamp = ""
-                
+
                 # Store the last location for distance calculations
                 last_location = location
-                
+
                 # Increment the counters
                 i += 1
                 n_goals += 1
-            
+
                 # Set up the next goal location
                 self.goal = MoveBaseGoal()
                 self.goal.target_pose.pose = locations[location]
                 self.goal.target_pose.header.frame_id = 'map'
                 self.goal.target_pose.header.stamp = rospy.Time.now()
-                
+
                 # Let the user know where the robot is going next
                 rospy.loginfo("Going to: " + str(location))
-                
+
                 # Start the robot toward the next location
                 self.move_base.send_goal(self.goal)
-                
+
                 # Allow 5 minutes to get there
-                finished_within_time = self.move_base.wait_for_result(rospy.Duration(300)) 
-                
+                finished_within_time = self.move_base.wait_for_result(rospy.Duration(300))
+
                 # Check for success or failure
                 if not finished_within_time:
                     self.move_base.cancel_goal()
@@ -266,19 +260,19 @@ class NavTest():
                         rospy.loginfo("State:" + str(state))
                     else:
                         rospy.loginfo("Goal failed with error code: " + str(goal_states[state]))
-                
+
                 # How long have we been running?
                 running_time = rospy.Time.now() - start_time
                 running_time = running_time.secs / 60.0
-                
+
                 # Print a summary success/failure, distance traveled and time elapsed
-                rospy.loginfo("Success so far: " + str(n_successes) + "/" + 
-                            str(n_goals) + " = " + 
+                rospy.loginfo("Success so far: " + str(n_successes) + "/" +
+                            str(n_goals) + " = " +
                             str(100 * n_successes/n_goals) + "%")
-                rospy.loginfo("Running time: " + str(trunc(running_time, 1)) + 
+                rospy.loginfo("Running time: " + str(trunc(running_time, 1)) +
                             " min Distance: " + str(trunc(distance_traveled, 1)) + " m")
                 rospy.sleep(self.rest_time)
-            
+
     def update_initial_pose(self, initial_pose):
         self.initial_pose = initial_pose
 
@@ -293,10 +287,10 @@ class NavTest():
     def callback(self, data):
         global movement_detected
         self.move_base.cancel_goal()
-        
+
         movement_detected = True
         rospy.loginfo(movement_detected)
-    
+
 
 def trunc(f, n):
     # Truncates/pads a float f to n decimal places without rounding
